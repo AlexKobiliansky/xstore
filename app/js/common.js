6d1a8cc6bfa9d36a2bd35bc0261cb4e504de3986
alex <alex_kobilianskiy@bigmir.net>
2020-02-19 18:29:01 +0200

    initial commit

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+
+        });
+        return false;
+    });
+});
